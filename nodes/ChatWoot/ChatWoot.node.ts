6c3019b1342722d8e88c638bf7450b5b32154d3a
Hugo Castro de Deco <hugodeco@sufficit.com.br>
2022-10-26 01:21:36 -0300

    contactCreate missing accountid

--- nodes/ChatWoot/ChatWoot.node.ts
+++ nodes/ChatWoot/ChatWoot.node.ts
@@ -132,7 +132,7 @@ export class ChatWoot implements INodeType {
 				displayOptions: {
 					show: {
 						resource: ['account', 'contact'],
-						operation: ['accountInformation', 'contactUpdate'],
+						operation: ['accountInformation','contactUpdate','contactCreate'],
 					},
 				},
 				default: '',
